bd55fd3f544fcb1672b014770a096d13e76dc38a
hrivuprasunhalder <hrivuprasunhalder@gmail.com>
2025-12-31 21:48:45 +0600

    Works on Material Science Division, Atomic Energy Center Dhaka
    Only the first charging phase is studied

---
+++ AECD,MSD-1/CO2 response of ZrOCl2 thin film/ZrOCl2_gas_sensing_RvT.m
@@ -0,0 +1,7 @@
+time= 0:15:420;
+resistance= [5.75 8.7 10.5 11.8 12.6 13.6 14.4 14.8 15.42 15.9 17.3 17.05 17.11 17.35 17.85 18.3 18.8 20.25 20.21 20.82 20.41 21.05 21.48 20.86 20.6 20.54 21.00 21.18 21.3];
+figure;
+plot(time,resistance);
+xlabel("Time(s)");
+ylabel("Response(Giga Ohm) ");
+title("CO2 Response on ZrOCl2 thin film(spin-coated by noobs)");
